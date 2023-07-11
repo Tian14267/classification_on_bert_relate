2ff1aa5acec5ff2d3a6e0d67bb5f5a7ac21a508e
Tian14267 <tian14267@163.com>
2023-07-11 14:38:54 +0800

    Add files via upload

---
+++ pytorch_code/models/albert_models/__init__.py
@@ -0,0 +1,15 @@
+#from .tokenization_albert import AlbertTokenizer
+from .tokenization import BertTokenizer, BasicTokenizer, WordpieceTokenizer
+from .modeling_albert import (
+            ALBERT_PRETRAINED_MODEL_ARCHIVE_LIST,
+            AlbertForMaskedLM,
+            AlbertForMultipleChoice,
+            AlbertForPreTraining,
+            AlbertForQuestionAnswering,
+            AlbertForSequenceClassification,
+            AlbertForTokenClassification,
+            AlbertModel,
+            AlbertPreTrainedModel,
+            load_tf_weights_in_albert,
+        )
+
